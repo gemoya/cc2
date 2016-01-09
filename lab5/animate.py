86162bd27c4e6ffe1e5a1e115b66e614609491df
asanhuez <asanhuez@alumnos.inf.utfsm.cl>
2016-01-09 02:45:39 -0300

    wea de lab

--- lab5/animate.py
+++ lab5/animate.py
@@ -1,10 +1,7 @@
-"""
-> sol must be a 3D numpy array, with dimensions (Nt,Nx,Ny)
-> time_step:  miliseconds to update canvas
-> frame_step: Jump step for frames to show
-"""
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
 def animate_pattern(sol, time_step=10, frame_step=20):
-    #time index
     t_ind = 0
     fig = plt.figure()
     im = plt.imshow(sol[t_ind], cmap=plt.cm.winter)
@@ -20,5 +17,5 @@ def animate_pattern(sol, time_step=10, frame_step=20):
     
     #animate it!
     ani = animation.FuncAnimation(fig, updatefig, frames=frames, 
-                    interval=time_step, blit=True, repeat=False)
+                    interval=time_step, blit=True, repeat=True)
     plt.show()
